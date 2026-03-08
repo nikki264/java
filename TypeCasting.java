be422f9c887db1dc151dc426f47b71badd9716b7
nikki264 <nikhila2815@gmail.com>
2026-03-08 22:03:08 +0530

    Create TypeCasting.java

---
+++ TypeCasting.java
@@ -0,0 +1,7 @@
+class TypeCasting {
+    public static void main(String[] args) {
+        int a = 15;
+        float b = (float)a;
+        System.out.println("Integer " + a + " typecaste in to float as " + b );
+    }
+}
